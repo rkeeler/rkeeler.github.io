9b5d2f9e0310c539ece914757495218561bfea98
Reagan Keeler <6854684+rkeeler@users.noreply.github.com>
2022-08-03 03:08:24 +0000

    test posts

---
+++ app/routes/posts/post-a.tsx
@@ -0,0 +1,8 @@
+export default function Post() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Post A</h1>
+      <p>test</p>
+    </main>
+  );
+}
---
+++ app/routes/posts/post-b.tsx
@@ -0,0 +1,8 @@
+export default function Post() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Post B</h1>
+      <p>test</p>
+    </main>
+  );
+}
